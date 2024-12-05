d6c8ad4f00290f4d04ead95fc19f18c6694cf956
[alireza azizi] <[alirezaazizi.offficial.a@gmail.com]>
2024-12-05 17:07:48 +0330

    /

--- core/settings.py
+++ core/settings.py
@@ -85,5 +85,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from store import urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, documentroot = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, documentroot = settings.MEDIA_ROOT)
