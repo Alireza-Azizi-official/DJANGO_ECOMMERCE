50b9a6f07f57efc5a81ed77c5ac343806fa16b20
[alireza azizi] <[alirezaazizi.offficial.a@gmail.com]>
2024-12-05 15:42:40 +0330

    .

--- core/settings.py
+++ core/settings.py
@@ -84,3 +84,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'medai/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
