e9521f62a7e42ae909509c3f4f1896fbd078723f
[alireza azizi] <[alirezaazizi.offficial.a@gmail.com]>
2024-12-06 00:07:36 +0330

    .

--- core/settings.py
+++ core/settings.py
@@ -42,7 +42,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'store.context_processors.menu_links'
+                'store.context_processors.menu_link'
             ],
         },
     },
--- store/views.py
+++ store/views.py
@@ -4,15 +4,24 @@ from .models import Category, Product
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
-def home(request, category_slug=None):
+def home(request, category_slug = None):
     category_page = None
     products = None
-    if category_slug != None:
-        category_slug = get_object_or_404(Category, slug = category_slug)
+    
+    if category_slug:
+        category_page = get_object_or_404(Category, slug = category_slug)
         products = Product.objects.filter(category = category_page, available = True)
     else:
-        products = Product.objects.all().filter(available = True)
-    return render(request,'home.html',{'category_page': category_page, 'products': products})
+        products = Product.objects.filter(available = True)
+        
+    categories = Category.objects.all()
+    return render(request, 'home.html', {
+        'category_page': category_page,
+        'products':products,
+        'links':categories
+    })
+
+
 
 def product_page(request):
     return render(request,'product.html')
