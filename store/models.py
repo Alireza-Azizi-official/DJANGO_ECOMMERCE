2a633a34a92e4cc5c2ca71a057af7c41f57d8f92
[alireza azizi] <[alirezaazizi.offficial.a@gmail.com]>
2024-12-05 16:56:29 +0330

    .

--- store/models.py
+++ store/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from . import views
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='category', blank=True)
+    
+    def __str__(self) :
+        return self.name
