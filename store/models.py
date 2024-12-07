38fcd4189459bcc221e743248fd3a59029c604bc
[alireza azizi] <[alirezaazizi.offficial.a@gmail.com]>
2024-12-07 11:03:36 +0330

    .

--- store/models.py
+++ store/models.py
@@ -38,6 +38,7 @@ class Product(models.Model):
         
     def get_url(self):
         return reverse('product_detail', args=[self.category.slug, self.slug])
+
     
     def __str__(self):
         return self.name
